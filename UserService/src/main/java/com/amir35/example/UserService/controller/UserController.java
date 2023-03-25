f75fefcd92ee13c37dd072269f5dd87aa02d21dc
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2023-03-26 03:37:11 +0530

    Added Circuit breaker resilience4j functionality

--- UserService/src/main/java/com/amir35/example/UserService/controller/UserController.java
+++ UserService/src/main/java/com/amir35/example/UserService/controller/UserController.java
@@ -3,6 +3,9 @@ package com.amir35.example.UserService.controller;
 import com.amir35.example.UserService.dto.UserDto;
 import com.amir35.example.UserService.entities.User;
 import com.amir35.example.UserService.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     //create
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDto user){
@@ -26,9 +31,26 @@ public class UserController {
 
     //single user
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //creating fallback method for circuit breaker
+    //make sure Return type should be same as that of the calling method --> public ResponseEntity<User> getSingleUser
+    //so here it is ResponseEntity<User>
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+        logger.info("Fallback is executed because service is down : ", ex.getMessage());
+
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This is dummy user")
+                .userId("12345")
+                .build();
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 
